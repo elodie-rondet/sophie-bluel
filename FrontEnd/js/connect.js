f53a26458fafeaac785bf5c50006338045fb4af8
Elodie RONDET <49192803+melisandria@users.noreply.github.com>
2023-06-02 18:20:55 +0200

    Update connect.js

--- FrontEnd/js/connect.js
+++ FrontEnd/js/connect.js
@@ -1,27 +1,43 @@
 function connexion() {
-    const email = document.querySelector('#email2').value;
-    const password = document.querySelector('#mot-de-passe').value;
-    fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: {
-          'Authorization': `Bearer ` + sessionStorage.getItem('token'),
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            email, password 
-        }),
-      })
-        .then((response) => response.json())
-        .then(function (response) {{ 
-          if (response.token != undefined) {
-                sessionStorage.setItem('token', response.token);   
-                document.location.href="homepage_edit.html"; 
-          }
-          else
-          alert('login ou mot de passe erroné');
+  const email = document.querySelector('#email2').value;
+  const password = document.querySelector('#mot-de-passe').value;
+  fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        'Authorization': `Bearer ` + localStorage.getItem('token'),
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+          email, password 
+      }),
+    })
+      .then((response) => response.json())
+      .then(function (response) {{ 
+        if (response.token != undefined) {
+          localStorage.setItem('token', response.token);   
+          document.location.href="index.html"; 
         }
-          });
-  }
+        else
+        alert('login ou mot de passe erroné');
+      }
+        });
+}
+
+function deconnexion() {
+var dialog = confirm("Souhaitez-vous vous déconnecter?");
+if (dialog) {
+localStorage.removeItem('token');
+document.location.href="index.html"; 
+}
+else {
+document.querySelector("body#body").setAttribute("style","margin-top:5%");
+document.querySelector("li#lien-login").innerHTML="logout";
+document.querySelector("li#lien-login").setAttribute("onclick","deconnexion()");
+}
+}
+
+
+
 
 
 
